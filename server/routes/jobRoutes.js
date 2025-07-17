cdd9f45de9ee46b1d0faefb8746f501adc377bcd
HimanshuRana9 <167887418+HimanshuRana9@users.noreply.github.com>
2025-07-17 05:40:05 +0530

    Update jobRoutes.js

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createJob, getJobs } = require('../controllers/jobController');
+const { createJob, getAllJobs } = require('../controllers/jobController');
 
-// @route   POST /api/jobs
-// @desc    Create a new job posting
 router.post('/', createJob);
-
-// @route   GET /api/jobs
-// @desc    Get all job postings
-router.get('/', getJobs);
+router.get('/', getAllJobs);
 
 module.exports = router;
+
